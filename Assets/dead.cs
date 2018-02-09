d2bd1c1f39eced0055de26d871fe17c3e296452c
risonakamo <hmykanae@gmail.com>
2018-02-09 00:18:59 -0500

    initial commit its bad

---
+++ Assets/dead.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dead:MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("hey2");
+    }
+}
---
+++ Assets/huhh.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class huhh:MonoBehaviour
+{
+    public Rigidbody2D body;
+    Vector2 m_velocity;
+    float m_speedModifier;
+
+    void Start()
+    {
+        body=GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            m_speedModifier=4;
+        }
+
+        else
+        {
+            m_speedModifier=12;
+        }
+
+        m_velocity.Set(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+
+        if (body.position.x>18 && m_velocity.x>0)
+        {
+            m_velocity.x=0;
+        }
+
+        if (body.position.x<-18 && m_velocity.x<0)
+        {
+            m_velocity.x=0;
+        }
+
+        m_velocity.Normalize();
+
+        body.velocity=m_velocity*m_speedModifier;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("hey");
+    }
+}
