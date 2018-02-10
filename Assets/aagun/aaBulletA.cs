0576ff34e2b4436bd06a4337ff7f2e20aa60d511
risonakamo <hmykanae@gmail.com>
2018-02-10 13:17:28 -0500

    exploding aa gun first try

--- Assets/aagun/aaBulletA.cs
+++ Assets/aagun/aaBulletA.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//aa bullet alpha, big bullet fired by gun
 public class aaBulletA:MonoBehaviour
 {
     Vector2 m_direction;
+
     float m_velocity=.1f;
     float m_minVelocity=.01f;
-    float m_decceleration=.03f;
+    float m_decceleration=.01f;
+
+    public GameObject aaBulletB;
+
     // float m_spawnTime;
 
     void Start()
@@ -16,7 +21,7 @@ public class aaBulletA:MonoBehaviour
         m_direction.Normalize();
 
         InvokeRepeating("updateVelocity",0,.5f);
-
+        Invoke("explode",3);
         // m_spawnTime=Time.time;
     }
 
@@ -36,5 +41,29 @@ public class aaBulletA:MonoBehaviour
         {
             m_velocity=m_minVelocity;
         }
+    }
+
+    void explode()
+    {
+        GameObject newBullet;
+        aaBulletB bulletComp;
+
+        newBullet=Instantiate(aaBulletB,transform.position,new Quaternion()) as GameObject;
+        bulletComp=newBullet.GetComponent<aaBulletB>();
+        bulletComp.setDirection(new float[2]{0,1});
+
+        newBullet=Instantiate(aaBulletB,transform.position,new Quaternion()) as GameObject;
+        bulletComp=newBullet.GetComponent<aaBulletB>();
+        bulletComp.setDirection(new float[2]{0,-1});
+
+        newBullet=Instantiate(aaBulletB,transform.position,new Quaternion()) as GameObject;
+        bulletComp=newBullet.GetComponent<aaBulletB>();
+        bulletComp.setDirection(new float[2]{1,0});
+
+        newBullet=Instantiate(aaBulletB,transform.position,new Quaternion()) as GameObject;
+        bulletComp=newBullet.GetComponent<aaBulletB>();
+        bulletComp.setDirection(new float[2]{-1,0});
+
+        Destroy(this.gameObject);
     }
 }
---
+++ Assets/aagun/aaBulletB.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//aa bullet Beta, small bullet fired by exploding aa bullet Alpha
+public class aaBulletB:MonoBehaviour
+{
+    public Vector2 m_direction;
+    public float m_speed=.1f;
+
+    void Start()
+    {
+        // m_direction=new Vector2(1,0);
+    }
+
+    void Update()
+    {
+        transform.Translate(m_direction*m_speed);
+    }
+
+    public void setDirection(float[] direction)
+    {
+        m_direction=new Vector2(direction[0],direction[1]);
+    }
+
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        if (trigger.tag=="bulletcatcher")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
